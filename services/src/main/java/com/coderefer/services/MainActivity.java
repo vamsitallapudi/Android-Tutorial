7666e5dcdd9ce3e0dbeb9f5ddf31440f87552853
Vamsi Tallapudi <vamsi@coderefer.com>
2018-09-24 09:31:10 +0530

    adding new module called services

---
+++ services/src/main/java/com/coderefer/services/MainActivity.java
@@ -0,0 +1,13 @@
+package com.coderefer.services;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
